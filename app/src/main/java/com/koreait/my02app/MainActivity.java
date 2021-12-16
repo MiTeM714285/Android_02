2f891e047ddf0cba9ca77978d5c1d45af5a38624
MiTeM714285 <mitem714285@naver.com>
2021-12-16 17:48:18 +0900

    commit 2021-12-16 TextView setText해보기

--- app/src/main/java/com/koreait/my02app/MainActivity.java
+++ app/src/main/java/com/koreait/my02app/MainActivity.java
@@ -3,6 +3,9 @@ package com.koreait.my02app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tvHello = findViewById(R.id.helloTv);
+        tvHello.setText("텍스트뷰 글자 설정하기");
+
     }
 }
